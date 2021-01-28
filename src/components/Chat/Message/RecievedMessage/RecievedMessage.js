c2150a64555aee3de702672c148015d09a12983f
ankitbrahmbhatt1997 <ankitbrahmbhatt1997@gmail.com>
2021-01-28 20:53:17 +0530

    Readme improved

--- src/components/Chat/Message/RecievedMessage/RecievedMessage.js
+++ src/components/Chat/Message/RecievedMessage/RecievedMessage.js
@@ -10,8 +10,14 @@ const useStyles = makeStyles((theme) => ({
   },
   root: {
     borderRadius: "0 10px 10px 10px",
+
+    background: theme.palette.primary.mainGradient,
     boxShadow:
       "10px 10px 50px rgba(42, 139, 242, 0.1), 15px 15px 35px rgba(42, 139, 242, 0.05), 10px 10px 25px rgba(42, 139, 242, 0.1)",
+    color: "#fff",
+  },
+  text: {
+    color: "white",
   },
   time: {
     color: "rgba(112, 124, 151, 0.7)",
@@ -32,7 +38,9 @@ export default function RecievedMessage({
         <Box width="1rem"></Box>
         <Box textAlign="end">
           <Box p="0.7rem" className={classes.root}>
-            <Typography variant="subtitle1">{content}</Typography>
+            <Typography className={classes.text} variant="subtitle1">
+              {content}
+            </Typography>
           </Box>
           <Typography variant="caption" className={classes.time}>
             {parsedTimestamp}
